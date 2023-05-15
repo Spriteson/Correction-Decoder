194bdbf73bf5451fd31fc21543d8bcdf726b4e4e
Spriteson <107821479+Spriteson@users.noreply.github.com>
2023-05-14 18:08:35 -0700

    Update reg_file.sv

--- verilog/reg_file.sv
+++ verilog/reg_file.sv
@@ -7,7 +7,7 @@ module reg_file #(parameter pw=4)(
   input[pw-1:0] wr_addr,		  // write address pointer
               rd_addrA,		  // read address pointers
 			  rd_addrB,
-			  regMem
+			  regMem,
   output logic[7:0] datA_out, // read data
                     datB_out);
 
